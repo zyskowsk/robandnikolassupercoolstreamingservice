1a6d89f10deccf860f1027673027be43d243c02f
Rob Zyskowski <zyskowski.rob@gmail.com>
2018-07-20 11:23:32 +0200

    everything totally works

--- client.go
+++ client.go
@@ -1,32 +1,63 @@
 package main
 
-import {
+import (
     "fmt"
     "net/http"
+	"log"
+	"os"
+	"io/ioutil"
+	"encoding/json"
+	"strconv"
+)
+
+var SONG_ID_GET_PARAM = "id"
+var SERVER_HOST = "localhost"
+var SERVER_PORT = 8080
+
+func get_cached_song_by_id(song_id int) []byte {
+	bytes := map[int][]byte{
+		123: []byte("firstsongbytes"),
+		345: []byte("secondsongbytes"),
+	}
+	fmt.Printf("song_id = %d\n", song_id)
+	song_bytes := bytes[song_id]
+
+	fmt.Printf("song_bytes = %s\n", string(song_bytes))
+	if song_bytes == nil {
+		return []byte{}
+	}
+	return song_bytes
 }
 
+func serve_cached_song_handler(w http.ResponseWriter, r *http.Request) {
+	song_id, err := strconv.Atoi(r.URL.Query()[SONG_ID_GET_PARAM][0])
+	if err != nil {
+		os.Exit(2)
+	}
+
+	var song_bytes = get_cached_song_by_id(song_id)
+	fmt.Fprintf(w, string(song_bytes))
+}
+
+func get_song(song_id int) {
+	url := fmt.Sprintf("http://%s:%d?%s=%d", SERVER_HOST, SERVER_PORT, SONG_ID_GET_PARAM, song_id)
+	fmt.Printf("url = %s\n", url)
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		os.Exit(2)
+	}
+
+	defer response.Body.Close()
+	var ips map[string]interface{}
+	body_content, _ := ioutil.ReadAll(response.Body)
+	json.Unmarshal(body_content, &ips)
+	fmt.Printf("ips = %s", ips)
+}
 
 func main() {
-    lc := make(chan string, 10)
-    exit := make(chan bool)
+	get_song(123)
 
-    go func () {
-        for {
-            var line string
-            fmt.Scanf("%s", &line)
-            lc <- line
-
-            if line == "exit" {
-                exit <- true
-            }
-        }
-    }()
-
-    go func() {
-        for {
-            fmt.Printf(<-lc)
-        }
-    }()
-
-    <-exit
+    http.HandleFunc("/cached-song", serve_cached_song_handler)
+    log.Fatal(http.ListenAndServe(":4002", nil))
 }
--- server.go
+++ server.go
@@ -4,10 +4,37 @@ import (
     "fmt"
     "net/http"
 	"log"
+	"strconv"
+	"os"
+	"encoding/json"
 )
 
+var SONG_ID_GET_PARAM = "id"
+
+func get_song_hosts(song_id int) []string {
+	ips := map[int][]string{
+		123: []string{"127.0.0.1"},
+		345: []string{"123.4.5.6", "456.12.21.12"},
+	}
+	song_ips := ips[song_id]
+	if song_ips == nil {
+		return []string{}
+	}
+	return song_ips
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	var song_id_str = r.URL.Query()[SONG_ID_GET_PARAM][0]
+	song_id, err := strconv.Atoi(song_id_str)
+	if err != nil {
+		os.Exit(2)
+	}
+
+	fmt.Printf("Request comming, id = %d\n", song_id)
+	var song_hosts = get_song_hosts(song_id)
+	response, _ := json.Marshal(song_hosts)
+
+	fmt.Fprintf(w, string(response))
 }
 
 func main() {
